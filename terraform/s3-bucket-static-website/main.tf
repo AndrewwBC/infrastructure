bd6e6563e8fba99093d16641a0e2315c56509b6d
AndrewwBC <andrew-hot2011@hotmail.com>
2024-02-11 10:36:04 -0300

    tired of bugs

--- terraform/s3-bucket-static-website/main.tf
+++ terraform/s3-bucket-static-website/main.tf
@@ -12,7 +12,7 @@ resource "aws_s3_bucket" "static_site_bucket" {
 
     website {
       index_document = "index.html"
-      error_document = 404.html
+      error_document = "404.html"
     }
 
     tags = {
